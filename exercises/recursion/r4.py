10cc471d14c8faa0fa5ac8b68513f3663a132ab1
Thanathat Pinthu <thanathatp2003@gmail.com>
2023-08-20 19:01:01 +0700

    Finish recursion#4

--- exercises/recursion/r4.py
+++ exercises/recursion/r4.py
@@ -1,21 +1,22 @@
-def pantip(k, n, arr, path, patt):
-    print(f"n : {n}, path : {path}, patt : {patt}")
-    if n > len(arr) - 1:
-        return len(d.keys())
-    elif patt + str(n) in d.keys():
-        print(f"patt + str(n) : {patt + str(n)}, dict_key : {d.keys()}")
+def pantip(k, n, arr, path, patt): # Using dict to keep the pattern as a key and to keep the path as a value for the amount of patterns
+    # print(f"n : {n}, path : {path}")
+    if path == [] and n == len(arr) - 1: # If path is empty and n is the last index of arr
+        if arr[n] == k:
+            d[patt + ' ' + str(n)] = [arr[n]]
+            print(arr[n]) 
+        return len(d.keys()) # return length of dict keys list for patterns
+    elif n > len(arr) - 1: # If n is greater than last index of arr, then pop the last element of path and recall function with new parameter
+        path.pop()
+        patt_list = patt.split(' ')
+        return pantip(k, int(patt_list.pop()) + 1, arr, path, ' '.join(patt_list)) # New parameter n = index of pop element, remove the last patt number
+    elif sum(path) + arr[n] == k: # If sum of path + current product price == money, keep in dict and print path then recall function with parameter n + 1
+        d[patt + ' ' + str(n)] = path
+        print(f"{' '.join(list(map(str,path)))} {arr[n]}" if path != [] else arr[n])
         return pantip(k, n + 1, arr, path, patt)
-    elif sum(path) + arr[n] == k:
-        path.append(arr[n])
-        d[patt + str(n)] = path
-        print(f"path : {path}, dict_key : {d}")
-        print(' '.join(list(map(str, path))))
-        return pantip(k, 0, arr, [], "")
-    elif sum(path) + arr[n] < k:
-        path.append(arr[n])
-        print(f"path : {path}")
-        return pantip(k, n + 1, arr, path, patt + str(n))
-    return pantip(k, n + 1, arr, path, patt)
+    elif sum(path) + arr[n] < k: # If sum of path + current product price < money then recall function with parameter n + 1
+        return pantip(k, n + 1, arr, path + [arr[n]], patt + ' ' + str(n))
+    else:
+        return pantip(k, n + 1, arr, path, patt) # Moreover the above conditions, just recall function with parameter n + 1
 
 d = {}
 inp = input('Enter Input (Money, Product) : ').split('/')
